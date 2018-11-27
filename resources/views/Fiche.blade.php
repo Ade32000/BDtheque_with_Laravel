bb7c9f873b3421234490d6aa72483434ccc1e076
Adeline <adeline.contact@gmail.com>
2018-11-27 11:09:31 +0100

    modif fiche

--- resources/views/Fiche.blade.php
+++ resources/views/Fiche.blade.php
@@ -2,25 +2,50 @@
 
 @section('title')
 
-    Fiche produit
+	Fiche produit
 
 @endsection
 
 @section('content')
-    
 
 
-        @foreach ($bd as $bd)
+<div id="fiche_content" class="fiche">
 
-            <div>{{$bd->comic_title}}</div>
-            
-            @foreach ($image as $image)
+	<div id="fiche_left">
 
-                <div><img src="{{$image->image_src}}"><div>
-                    
-            @endforeach
+		@foreach ($bd as $bd)
 
-        @endforeach
+			<h2>{{$bd->comic_title}}</H2>
 
+				@foreach ($image as $image)
+
+					<img id="img_fiche" src="{{$image->image_src}}">
+
+					
+						<div id="resume">
+							
+							<h6>Résumé :</h6>
+
+							{{$bd->comic_resume}}
+
+						</div>
+
+				@endforeach
+
+		@endforeach
+
+		<button class="fiche" id="favorite" name="favorite">Ajouter à mes favoris</button>
+
+	</div>
+
+		
+	<div id="fiche_right">
+			
+		<h5>Dans la même collection :</h5>
+
+	</div>
+
+</div>
 
 @endsection
+
